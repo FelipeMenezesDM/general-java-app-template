0a72480453870e6407addac544557212dafc381b
FelipeMenezesDM <contato@felipemenezes.com.br>
2023-03-05 17:40:05 -0300

    feat: refatorações para uso de biblioteca de logging

--- app/src/main/java/br/com/felipemenezesdm/Controller.java
+++ app/src/main/java/br/com/felipemenezesdm/Controller.java
@@ -1,7 +1,5 @@
 package br.com.felipemenezesdm;
 
-import br.com.felipemenezesdm.infrastructure.log.LogHandler;
-import br.com.felipemenezesdm.infrastructure.log.LogPayload;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
