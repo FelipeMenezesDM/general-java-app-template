10663f14f7f68394f560c8984f8b3ab3ab5c961a
FelipeMenezesDM <contato@felipemenezes.com.br>
2023-03-08 00:02:15 -0300

    feat: ajustando nome do relatório de execução de testes

--- tests/src/test/java/RunCucumber.java
+++ tests/src/test/java/RunCucumber.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = {"configs", "steps"},
-        plugin = "json:target/cucumber-reports/cucumber.json",
+        plugin = "json:target/reports/application-test-report.json",
         snippets = CucumberOptions.SnippetType.CAMELCASE
 )
 public class RunCucumber {
